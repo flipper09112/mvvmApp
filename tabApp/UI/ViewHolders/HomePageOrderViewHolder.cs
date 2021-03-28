92afa28f778452c717032ce6f5695bca1f14c275
DESKTOP-TVRH34H\Utilizador <flipper09@live.com.pt>
2021-03-28 21:18:53 +0100

    add info to extra order card

--- tabApp/UI/ViewHolders/HomePageOrderViewHolder.cs
+++ tabApp/UI/ViewHolders/HomePageOrderViewHolder.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.V7.Widget;
@@ -21,12 +22,14 @@ namespace tabApp.UI.ViewHolders
         private TextView _orderTitle;
         private TextView _orderAddress;
         private TextView _orderDesc;
+        private TextView _extraOrderTotal;
 
         public HomePageOrderViewHolder(View itemView) : base(itemView)
         {
             _orderTitle = itemView.FindViewById<TextView>(Resource.Id.orderTitle);
             _orderAddress = itemView.FindViewById<TextView>(Resource.Id.orderAddress);
             _orderDesc = itemView.FindViewById<TextView>(Resource.Id.orderDesc);
+            _extraOrderTotal = itemView.FindViewById<TextView>(Resource.Id.extraOrderTotal);
         }
 
         internal void Bind((Client Client, ExtraOrder ExtraOrder) extraOrder, Core.HomeViewModel viewModel)
@@ -34,6 +37,8 @@ namespace tabApp.UI.ViewHolders
             _orderTitle.Text = "Encomenda para o cliente '"+ extraOrder.Client.Name + "'\nId: " + extraOrder.Client.Id;
             _orderAddress.Text = extraOrder.Client.Address.AddressDesc;
             _orderDesc.Text = viewModel.GetOrderDesc(extraOrder.ExtraOrder);
+            _extraOrderTotal.Text = extraOrder.ExtraOrder.IsTotal ? "Total" : "Extra";
+            _extraOrderTotal.SetTextColor(GetColorFromInteger(extraOrder.ExtraOrder.IsTotal ? Resource.Color.blue : Resource.Color.red));
         }
 
         internal void Bind((Client Client, ExtraOrder ExtraOrder) extraOrder, GlobalOrderViewModel viewModel)
@@ -41,12 +46,21 @@ namespace tabApp.UI.ViewHolders
             _orderTitle.Text = "Encomenda para o cliente '" + extraOrder.Client.Name + "'\nId: " + extraOrder.Client.Id;
             _orderAddress.Text = extraOrder.Client.Address.AddressDesc;
             _orderDesc.Text = viewModel.GetOrderDesc(extraOrder.ExtraOrder);
+            _extraOrderTotal.Text = extraOrder.ExtraOrder.IsTotal ? "Total" : "Extra";
+            _extraOrderTotal.SetTextColor(GetColorFromInteger(extraOrder.ExtraOrder.IsTotal ? Resource.Color.blue : Resource.Color.red));
         }
         internal void Bind((Client Client, ExtraOrder ExtraOrder) extraOrder, SnoozeViewModel viewModel)
         {
             _orderTitle.Text = "Encomenda para o cliente '" + extraOrder.Client.Name + "'\nId: " + extraOrder.Client.Id;
             _orderAddress.Text = extraOrder.Client.Address.AddressDesc;
             _orderDesc.Text = viewModel.GetOrderDesc(extraOrder.ExtraOrder);
+            _extraOrderTotal.Text = extraOrder.ExtraOrder.IsTotal ? "Total" : "Extra";
+            _extraOrderTotal.SetTextColor(GetColorFromInteger(extraOrder.ExtraOrder.IsTotal ? Resource.Color.blue : Resource.Color.red));
+        }
+        public Color GetColorFromInteger(int colorId)
+        {
+            int color = MainActivity.Instance.ApplicationContext.GetColor(colorId);
+            return Color.Rgb(Color.GetRedComponent(color), Color.GetGreenComponent(color), Color.GetBlueComponent(color));
         }
     }
 }
