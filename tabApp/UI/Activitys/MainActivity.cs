8e60755a3dcc556696bbc4650ea9f0fb71b01c31
DESKTOP-TVRH34H\Utilizador <flipper09@live.com.pt>
2022-03-09 18:55:58 +0000

    change-version

--- tabApp/UI/Activitys/MainActivity.cs
+++ tabApp/UI/Activitys/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.Remoting.Contexts;
+using System.Threading.Tasks;
 using Android;
 using Android.App;
 using Android.Content;
@@ -57,6 +58,8 @@ namespace tabApp
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // In this example OnReleaseAvailable is a method name in same class
+            //Distribute.ReleaseAvailable = OnReleaseAvailable;
             AppCenter.Start("090e6c4a-73b9-4ce9-ab0e-19a958a1504f", typeof(Analytics), typeof(Crashes), typeof(Distribute));
             SetContentView(Resource.Layout.activity_main);
 
@@ -98,6 +101,47 @@ namespace tabApp
             LocationEventCommand = new MvxCommand<Location>(LocationEventCmd);
             Instance = this;
         }
+
+        /*private bool OnReleaseAvailable(ReleaseDetails releaseDetails)
+        {
+            // Look at releaseDetails public properties to get version information, release notes text or release notes URL
+            string versionName = releaseDetails.ShortVersion;
+            string versionCodeOrBuildNumber = releaseDetails.Version;
+            string releaseNotes = releaseDetails.ReleaseNotes;
+            Uri releaseNotesUrl = releaseDetails.ReleaseNotesUrl;
+
+            // custom dialog
+            var title = "Version " + versionName + " available!";
+            Task answer;
+
+            // On mandatory update, user can't postpone
+            if (releaseDetails.MandatoryUpdate)
+            {
+                answer = Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install");
+            }
+            else
+            {
+                answer = Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install", "Maybe tomorrow...");
+            }
+            answer.ContinueWith((task) =>
+            {
+                // If mandatory or if answer was positive
+                if (releaseDetails.MandatoryUpdate || (task as Task<bool>).Result)
+                {
+                    // Notify SDK that user selected update
+                    Distribute.NotifyUpdateAction(UpdateAction.Update);
+                }
+                else
+                {
+                    // Notify SDK that user selected postpone (for 1 day)
+                    // This method call is ignored by the SDK if the update is mandatory
+                    Distribute.NotifyUpdateAction(UpdateAction.Postpone);
+                }
+            });
+
+            // Return true if you're using your own dialog, false otherwise
+            return true;
+        }*/
 
         protected override async void OnResume()
         {
